51a8baf3e2c1c56945c00844015fcd2729e97ae0
AOE Rules <AOE Rules@172.30.1.44>
2015-03-17 12:07:51 +0100

    First commit

---
+++ Git/src/Hola.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author AOE Rules
+ *
+ */
+public class Hola {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hola q ase");
+
+	}
+
+}
